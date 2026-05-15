b2b9997f190e7b207304cfa01c404a1da8d135c7
MirandaCapilloAndy <mirandacapilloandy@gmail.com>
2026-05-14 19:36:40 -0500

    feat: implementar factorial recursivo

---
+++ Factorial.java
@@ -0,0 +1,11 @@
+// V2: Implementación recursiva
+public class Factorial {
+
+    public static long calcular(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+
+        return n * calcular(n - 1);
+    }
+}
